266078f7af8608b86b2c70c9af586076509ca4f3
WoutDepeuter <123627621+WoutDepeuter@users.noreply.github.com>
2024-12-22 11:40:16 +0100

    aanmaken test file voor testen

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,7 @@
+
+<ul class="flex space-x-4 bg-gray-800 p-4 text-white">
+    <li><a href="/" class="hover:text-gray-400">Home</a></li>
+    <li><a href="/" class="hover:text-gray-400">About</a></li>
+    <li><a href="/" class="hover:text-gray-400">FAQ</a></li>
+    <li><a href="/" class="hover:text-gray-400">LOGIN</a></li>
+</ul>
---
+++ resources/views/test.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test</title>
+</head>
+<body class="bg-gray-100 text-gray-900">
+    <div class="container mx-auto p-4">
+        @include('components.navbar')
+    </div>
+</body>
+</html>
