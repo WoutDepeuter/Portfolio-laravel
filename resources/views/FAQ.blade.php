7b23780f3e94ae3dc7f3015501b65c86e4536a2e
depeu <depeuterwout@gmail.com>
2024-12-29 12:21:50 +0100

    aanpassen FAQ dat alleen admins kunnen toevoegen en dat als je ingelogd ben je naar profiel gaat inplaats van login

--- resources/views/FAQ.blade.php
+++ resources/views/FAQ.blade.php
@@ -30,10 +30,8 @@
     <x-FaqList :faqs="$faqs" />
 </div>
 
-<a href="{{ route('faqs.create') }}" class="fixed bottom-4 right-4 bg-blue-500 text-white px-4 py-2 rounded-full shadow-lg hover:bg-blue-600 transition duration-300">
-    Add FAQ
-</a>
-
+@auth
+@if(auth()->user()->role === 'admin')
 <!-- Button to toggle the category form -->
 <button onclick="toggleForm('categoryForm')" class="fixed bottom-4 left-4 mr-4 bg-blue-500 text-white px-4 py-2 rounded-full shadow-lg hover:bg-blue-600 transition duration-300">
     Add Category
@@ -79,6 +77,8 @@
         <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded-full shadow-lg hover:bg-blue-600 transition duration-300">Create FAQ</button>
     </form>
 </div>
+@endif
+@endauth
 
 </body>
 </html>
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -17,12 +17,21 @@
                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 0 1-2.25 2.25M16.5 7.5V18a2.25 2.25 0 0 0 2.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 0 0 2.25 2.25h13.5M6 7.5h3v3H6v-3Z" />
                 </svg>
             </a>
+            @auth
+            <a href="{{ route('profile.edit') }}" class="text-gray-300 hover:text-white flex items-center space-x-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-9 h-9">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                </svg>
+                <span>Profile</span>
+            </a>
+            @else
             <a href="/login" class="text-gray-300 hover:text-white flex items-center space-x-2">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-9 h-9">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                 </svg>
                 <span>Login</span>
             </a>
+            @endauth
         </div>
     </div>
 </nav>
