2c7e4da798088e717835563f49a09316d3ccbed5
depeu <depeuterwout@gmail.com>
2025-01-17 16:27:48 +0100

    aanvullen  seeder

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 <!-- resources/views/components/footer.blade.php -->
 <footer class="bg-gray-800 p-4 mt-8">
     <div class="container mx-auto text-center text-white">
-        <p>&copy; {{ date('Y') }} Foilhat. All rights reserved.</p>
+        <p>&copy; {{ date('Y') }} CODEHUB. All rights reserved.</p>
         <div class="flex justify-center space-x-4 mt-2">
             <a href="/privacy-policy" class="text-gray-300 hover:text-white">Privacy Policy</a>
             <a href="/terms-of-service" class="text-gray-300 hover:text-white">Terms of Service</a>
