9484acb61ada9c2ae2c5176e4e3a0cd61332b4ed
depeu <depeuterwout@gmail.com>
2025-01-17 13:26:41 +0100

    fixen probleem met routing wanneer je user opzocht vanuit homepage

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -22,6 +22,7 @@
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
+                        @if(Auth::check())
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
                             <div>{{ Auth::user()->name }}</div>
 
@@ -31,9 +32,11 @@
                                 </svg>
                             </div>
                         </button>
+                        @endif
                     </x-slot>
 
                     <x-slot name="content">
+                        @if(Auth::check())
                         <x-dropdown-link :href="route('profile.edit')">
                             {{ __('Profile') }}
                         </x-dropdown-link>
@@ -48,6 +51,7 @@
                                 {{ __('Log Out') }}
                             </x-dropdown-link>
                         </form>
+                        @endif
                     </x-slot>
                 </x-dropdown>
             </div>
@@ -74,6 +78,7 @@
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
+            @if(Auth::check())
             <div class="px-4">
                 <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
                 <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
@@ -95,6 +100,7 @@
                     </x-responsive-nav-link>
                 </form>
             </div>
+            @endif
         </div>
     </div>
 </nav>
